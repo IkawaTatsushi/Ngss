c72e6fc1d1bded23df00dc41c0b6b993d11d4cd2
tt <kusomusi5555@gmail.com>
2021-03-15 12:29:59 +0900

    function作成、適用

--- delete.php
+++ delete.php
@@ -4,14 +4,17 @@ require('function.php');
 if(isset($_SESSION['id'])) {
     $id = $_REQUEST['id'];
     $message = getPost($id);
-
-    if($message['user_id'] === $_SESSION['id']) {
+    if($message['user_id'] == $_SESSION['id']) {
         if(file_exists('picture/'.$message['picture'])){
             unlink('picture/'.$message['picture']);
         }
-        deletePost($id);
+        $stmt = deletePost($id);
+        if($stmt){
         header('Location: index.php');
         exit();
+        }else{
+            echo '削除に失敗しました';
+        }
     }
 }else{
     header('Location: index.php');
