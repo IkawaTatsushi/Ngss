258e8cac80567abd099064d2cfebb7d35af71a6a
ikawa tatsushi <kusomusi5555@gmail.com>
2021-05-12 19:12:33 +0900

    画像アップデート機能を修正

--- update.php
+++ update.php
@@ -1,20 +1,19 @@
 <?php
 require('function.php');
 $user_id = $_SESSION['id'];
-$url = 'update.php?update_id='.$user_id;
 $pageId = $_REQUEST['update_id'];
 if($user_id == $pageId){
 	$user = getUser($user_id);
-	if(!empty($_POST) && !empty($_FILES['image']['name'])){
+	if(!empty($_POST['name']) && !empty($_FILES['image']['name'])){
 		$name = $_POST['name'];
-		$image = date('YmdHis') . $_FILES['image']['name'];
+		$image = date('YmdHis').$_FILES['image']['name'];
 				move_uploaded_file($_FILES['image']['tmp_name'],
-				'user_img/'. $image);
+				'user_img/'.$image);
 		$update = updateAll($name,$image,$user_id);
 		header('Location: myPage.php?myPage_id='.$user_id);
 		exit();
 	}
-	if(!empty($_POST) && empty($_FILES['image']['name'])){
+	if(!empty($_POST['name']) && empty($_FILES['image']['name'])){
 		$name = $_POST['name'];
 		$update = update($name,$user_id);
 		header('Location: myPage.php?myPage_id='.$_SESSION['id']);
@@ -30,7 +29,7 @@ if($user_id == $pageId){
 		<div class="col-lg-6 offset-lg-3">
 			<div class="box box1">
 
-				<form role="form" action="" method="post">
+				<form role="form" action="" method="post" enctype="multipart/form-data">
 					<div class="form-group text-center">
 						<label class="label1" for="exampleInputEmail1">ユーザーネーム</label>
 						<input type="text" name="name" size="35" maxlength="255" class="form-control form-control1 text-center" value="<?php echo h($user['name']); ?>">
@@ -44,7 +43,7 @@ if($user_id == $pageId){
 					<div class="divider-form divider-form1"></div>
 
 					<div class="form-group text-center">
-						<label class="label1" for="exampleInputPassword1">変更後プロフィール画像</label><br>
+						<label class="label1">変更後プロフィール画像</label><br>
 						<input type="file" name="image" class="form-control-file" id="images">
 						<img id="previews" class="rounded-circle">
 					</div>
